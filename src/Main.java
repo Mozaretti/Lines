a0ba08ce033b65bdb374bd7026868eb920837e6a
Eвгений <mocard778@gmail.com>
2023-02-24 14:00:50 +0700

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
     }
-    //С именем firstName — для хранения имени.
-    //С именем middleName — для хранения отчества.
-    //С именем lastName — для хранения фамилии.
-    //С именем fullName — для хранения Ф. И. О. сотрудника в формате "Фамилия Имя Отчество".
-    //Выведите в консоль фразу: «ФИО сотрудника — …».
-    //
-    //В качестве данных для задачи используйте "Ivanov Ivan Ivanovich".
-
     public static void task1(){
         System.out.println("Задача 1");
         String firsName = "Ivan";
@@ -18,5 +11,25 @@ public class Main {
         String lastName = "Ivanov";
         String fullName = lastName + " " + firsName + " " + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
+        System.out.println();
+    }
+    //Для ежемесячного отчета и ведения документации бухгалтерии нужны Ф. И. О. сотрудников,
+    // полностью написанные заглавными буквами (верхним регистром).
+    //
+    //Напишите программу, которая изменит написание Ф. И. О. сотрудника
+    // с "Ivanov Ivan Ivanovich" на полностью заглавные буквы.
+    //
+    //В качестве строки с исходными данными используйте строку fullName.
+    //
+    //Результат программы выведите в консоль в формате: "Данные ФИО сотрудника
+    // для заполнения отчета — …"
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String firsName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firsName + " " + middleName;
+        System.out.println(fullName.toUpperCase());
     }
 }
